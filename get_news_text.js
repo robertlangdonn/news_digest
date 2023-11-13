18bc18168e227bdb5af4b7ea9e660e7ea15b18ff
Prasad Khake <prasad@paperd.ink>
2023-11-13 22:31:44 +0530

    Update get_news_text.js
    - makes a CSV of failed URLs
    - uses RapidAPI API key from the .env file
    - better log statements

--- get_news_text.js
+++ get_news_text.js
@@ -1,31 +1,34 @@
 const axios = require('axios');
 const fs = require('fs');
 const csv = require('csv-parser');
+const { Parser } = require('json2csv'); // Import json2csv to convert JSON to CSV
+require('dotenv').config();
+
 
 async function getText(url) {
   const options = {
     method: 'GET',
     url: 'https://text-extract7.p.rapidapi.com/',
-    params: {
-      url: url
-    },
+    params: { url: url },
     headers: {
-      'X-RapidAPI-Key': 'YOUR_API_KEY', //Replace this with your RapidAPI Key for this API service: https://rapidapi.com/altanalys/api/text-extract7
+      'X-RapidAPI-Key': process.env.RAPIDAPI_KEY, // Use the API key from the .env file
       'X-RapidAPI-Host': 'text-extract7.p.rapidapi.com'
     }
   };
 
   try {
     const response = await axios.request(options);
+    console.log(`Text extraction successful for URL #${processedCount + 1}`);
     return response.data;
   } catch (error) {
-    console.error(`Error processing URL ${url}: ${error.message}`);
+    console.error(`Error processing URL #${processedCount + 1}: ${url}: ${error.message}`);
     return null;
   }
 }
 
-const inputCsvFilePath = 'input.csv'; //make sure you have input.csv file with a header as "url" and then a list of links below it
-const outputJsonFilePath = 'scraped_news.json'; //this is the output file which saves text, metadata, etc for every link in json format
+const inputCsvFilePath = 'input.csv';
+const outputJsonFilePath = 'scraped_news.json';
+const failedUrlsCsvFilePath = 'failed_urls.csv'; // File path for failed URLs CSV
 
 const urls = [];
 let processedCount = 0;
@@ -41,19 +44,21 @@ fs.createReadStream(inputCsvFilePath)
   })
   .on('end', async () => {
     const results = [];
+    const totalUrls = urls.length;
+    console.log(`Total URLs to process: ${totalUrls}`);
 
     for (const url of urls) {
-      console.log(`Processing URL: ${url}`);
+      processedCount++;
+      console.log(`Processing URL #${processedCount}: ${url}`);
       const extractedText = await getText(url);
       if (extractedText !== null) {
         results.push({ url, extractedText });
-        console.log(`Data for URL ${url} saved successfully.`);
+        console.log(`Data for URL #${processedCount} saved successfully.`);
       } else {
-        console.log(`Failed to process URL: ${url}`);
+        console.log(`Failed to process URL #${processedCount}: ${url}`);
         failedCount++;
         failedUrls.push(url); // Add the failed URL to the array
       }
-      processedCount++;
     }
 
     // Save the results array to a JSON file
@@ -64,11 +69,11 @@ fs.createReadStream(inputCsvFilePath)
     console.log(`Total URLs processed: ${processedCount}`);
     console.log(`Total URLs failed: ${failedCount}`);
 
-    // Log failed URLs
+    // Save failed URLs to a CSV file
     if (failedCount > 0) {
-      console.log('Failed URLs:');
-      failedUrls.forEach((url) => {
-        console.log(url);
-      });
+      const json2csvParser = new Parser();
+      const csv = json2csvParser.parse(failedUrls.map(url => ({ url })));
+      fs.writeFileSync(failedUrlsCsvFilePath, csv);
+      console.log('Failed URLs saved to:', failedUrlsCsvFilePath);
     }
   });
